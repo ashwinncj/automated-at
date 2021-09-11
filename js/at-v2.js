1315f6fe8a3b9369f41f479f439a3d59ee670ad2
Ashwin Kumar <ashwinncj@gmail.com>
2021-09-11 16:24:41 +0530

    Updated v2 files with drag and drop.

---
+++ js/at-v2.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+    console.log( 'Automated AT V2 loaded.' );
+
+});
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function atDrop(ev) {
+    ev.preventDefault();
+    console.log('AT Drop init');
+    var data = ev.dataTransfer.getData("text");
+    ev.target.append( data );
+}
+
+function addATToList() {
+    let atId = 'single-test-' + new Date().getTime();
+    
+    // AT Single section.
+    let atSection = document.createElement('div');
+    atSection.className = 'at-single';
+    atSection.id = atId;
+
+    // AT Url related inputs
+    let atUrlSection = document.createElement('div');
+    let atUrl = document.createElement('input');
+    atUrlSection.className = 'single-at-url';
+    atUrl.value = $('#at-url').val();
+    let atUrlLabel = document.createElement('span');
+    atUrlLabel.innerHTML = 'Target URL: ';
+    atUrlSection.append( atUrlLabel, atUrl );
+    
+    // AT Group related inputs
+    let atGroupSection = document.createElement('div');
+    let atGroup = document.createElement('input');
+    atGroupSection.className = 'single-at-group';
+    atGroup.value = $('#at-group').val();
+    let atGroupLabel = document.createElement('span');
+    atGroupLabel.innerHTML = 'Target Group: ';
+    atGroupSection.append( atGroupLabel, atGroup );
+
+    // Add remove test button to the single test
+    let atRemoveSection = document.createElement( 'div' );
+    let atRemoveBtn = document.createElement( 'button' );
+    atRemoveBtn.innerHTML = 'Remove this test!';
+    atRemoveBtn.className = 'at-remove-btn';
+    atRemoveSection.append( atRemoveBtn );
+
+    // Drop bay section.
+    let dropBaySection = document.createElement( 'div' );
+    dropBaySection.id = atId + '-drop-bay';
+    dropBaySection.className = 'at-drop-bay';
+
+    // Break <br> element
+    let br = document.createElement('br');
+
+    // Append to the list.
+    atSection.append( atUrlSection, atGroupSection, atRemoveBtn, dropBaySection );
+    $('#at-tests-container').append(atSection);
+
+    // Apped drop controls to drop-bay.
+    $( '#' + atId + '-drop-bay' ).attr( 'ondrop', 'atDrop( event )' );
+    $( '#' + atId + '-drop-bay' ).attr( 'ondragover', 'allowDrop( event )' );
+}   
+
+function seeAT( ev ) {
+    let seeAtSection = document.createElement('div');
+    
+    ev.dataTransfer.setData("text", 'Stage5');
+}
+
+$(document).on('click', '.at-remove-btn', function() {
+    $(this).parent().remove();
+});
