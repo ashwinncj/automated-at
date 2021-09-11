d7643419d0e1b65090561578788b0ab329465220
Ashwin Kumar <ashwinncj@gmail.com>
2021-09-11 21:18:35 +0530

    Updated read at functionality.

--- js/at-v2.js
+++ js/at-v2.js
@@ -39,6 +39,15 @@ function addATToList() {
     atGroupLabel.innerHTML = 'Target Group: ';
     atGroupSection.append( atGroupLabel, atGroup );
 
+    // AT Description related inputs
+    let atDescSection = document.createElement('div');
+    let atDesc = document.createElement('input');
+    atDescSection.className = 'single-at-desc';
+    atDesc.value = $('#at-desc').val();
+    let atDescLabel = document.createElement('span');
+    atDescLabel.innerHTML = 'AT Description: ';
+    atDescSection.append( atDescLabel, atDesc );
+    
     // Add remove test button to the single test
     let atRemoveSection = document.createElement( 'div' );
     let atRemoveBtn = document.createElement( 'button' );
@@ -55,7 +64,7 @@ function addATToList() {
     let br = document.createElement('br');
 
     // Append to the list.
-    atSection.append( atGroupSection, atRemoveBtn, dropBaySection );
+    atSection.append( atDescSection, atGroupSection, atRemoveBtn, dropBaySection );
     $('#at-tests-container').append(atSection);
 
     // Apped drop controls to drop-bay.
@@ -106,4 +115,66 @@ function onPageAtMarkup() {
     onPageAtMarkup.setAttribute( 'data-at-type', 'page' );
     onPageAtMarkup.innerHTML = '<div class="page"><span>Target URL? </span><input type="text"></div><span class="at-remove-btn x-btn">&#x2715</span>';
     return onPageAtMarkup;
+}
+
+$(document).on('click', '#at-download', function() {
+    let data = '';
+    data += extractAts();
+    console.log( data );
+});
+
+function extractAts() {
+    let data = '';
+    $('.at-single').each( function() {
+        data += extractAtGroups( this );
+        data += extractAtSectionName( this );
+        data += extractAtsFromDropBay( this );
+        data += getSectionEnding();
+    });
+    return data;
+}
+
+function extractAtsFromDropBay( dropBay ) {
+    let droppedElements = $(dropBay).children('.at-drop-bay').children('div');
+    let ats = '';
+    $(droppedElements).each( function() {
+        let atType = $(this).data('at-type');
+        if( 'click' == atType ) {
+            ats += extractClickAt( this );
+        }
+        if( 'page' == atType ) {
+            ats += extraactPageAt( this );
+        }
+    });
+    return ats;
+}
+
+function extractClickAt( el ) {
+    let data = $(el).children('.click').children('input').val();
+    if ( '' == data ) {
+        return '';
+    }
+    let at = '$I->click(\''+ data +'\');\n';
+    return at;
+}
+
+function extraactPageAt( el ) {
+    let data = $(el).children('.page').children('input').val();
+    if ( '' == data ) {
+        return '';
+    }
+    let at = '$I->amOnPage(\''+ data +'\');\n';
+    return at;
+}
+
+function extractAtSectionName( el ) {
+    console.log( el );
+}
+
+function getSectionEnding() {
+    return '}';
+}
+
+function extractAtGroups( el ) {
+    return '';
 }
